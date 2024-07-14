750657e9ede2e0c75f285b3eb42ec9857522602b
Tim Vasilev <timur.vasilev@paybis.com>
2024-07-14 19:50:28 +0300

    Add functional

--- src/Client/MailClient.php
+++ src/Client/MailClient.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace App\Client;
 
 use App\Service\MailSenderInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Throwable;
 
 class MailClient implements MailSenderInterface
 {
     public function __construct(
         private readonly MailerInterface $mailer,
+        private readonly LoggerInterface $logger
     ) {
     }
 
@@ -26,6 +29,10 @@ class MailClient implements MailSenderInterface
             ->subject($subject)
             ->text($text);
 
-        $this->mailer->send($email);
+        try {
+            $this->mailer->send($email);
+        } catch (Throwable $e) {
+            $this->logger->error('Error on sending email: ' . $e->getMessage(), ['exception' => $e]);
+        }
     }
 }
